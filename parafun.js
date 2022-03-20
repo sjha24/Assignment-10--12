b9122c7a371754516dc3bc20bb193eb14e65bcb1
Saurav <98340874+sjha24@users.noreply.github.com>
2022-03-20 16:14:34 +0530

    Add files via upload

---
+++ parafun.js
@@ -0,0 +1,40 @@
+function fnAdd (x , y) {
+let a = x ;
+let b = y ;
+let r = x+y ;
+console.log (r) ;
+return r
+}
+
+let r= fnAdd(30 ,20) ;
+
+
+function fnSub (x ,y) {
+    let a = x ;
+    let b = y ;
+    let r = x-y ;
+    console.log (r)
+    return r
+}
+fnSub (50,30) ;
+
+function fnMul(x , y) {
+    let a = x ;
+    let b = y ;
+    let r = x * y ;
+    console.log (r)
+    return r
+}
+fnMul (20, 30)
+
+
+function fnDiv(x, y) {
+    let a = x ;
+    let b = y ;
+    let r = x / y ;
+    console.log (r)
+    return r
+}
+fnDiv (200, 20)
+
+
